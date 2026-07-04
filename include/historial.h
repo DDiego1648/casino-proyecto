1e49830044988f533d8e9b883538bb21b54a9834
MaximoMelgar679 <00154926@uca.edu.sv>
2026-07-03 22:30:37 -0600

    Add file reading functionality in historial.h

--- include/historial.h
+++ include/historial.h
@@ -1,1 +1,18 @@
-//historial
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ifstream archivo("historial.txt");
+    string linea;
+
+    while (getline(archivo, linea)) {
+        cout << linea << endl;
+    }
+
+    archivo.close();
+
+    return 0;
+}
